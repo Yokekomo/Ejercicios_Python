4cd92b031e965774296990e424bd68fa2b04a09e
Yokekomo <albanochicharroaltur@gmail.com>
2022-01-10 15:39:39 +0100

    Ejercicios Python basico

---
+++ ejercicios_01_20.py
@@ -0,0 +1,26 @@
+# Ejercicio 01: Mostrar la versión actual de Python instalada.
+# Ejercicio 02: Exponer el uso basico de la funcion print().
+# Ejercicio 03: Obtener la Fecha y la Hora actuales del sistema con el módulo datetime.
+# Ejercicio 04: Solicitar el valor del radio de un círculo para calcular su área.
+# Ejercicio 05: Obtener la representación inversa de una cadena de caracteres.
+# Ejercicio 06: Obtener un conjunto de números separados por coma y crear una lista.
+# Ejercicio 07: Obtener el nombre de extension de un nombre de archivo.
+# Ejercicio 08: Obtener el primer y último elemento de una lista.
+# Ejercicio 09: Mostrar la fecha de un evento almacenada en una tupla.
+# Ejercicio 10: Solicita al usuario un número n y calcular n + nn + nnn.
+# Ejercicio 11: Obtener la documentación de funciones incorporadas.
+# Ejercicio 12: Imprimir el calendario para un mes y año específico.
+# Ejercicio 13: Crear una cadena de caracteres multilínea.
+# Ejercicio 14: Calcular la diferencia en día de dos fechas dadas.
+# Ejercicio 15: Calcular el volumen de una esfera a partir del radio dado.
+# Ejercicio 16: Crear una función para evaluar un número y realizar una operación
+# fn(n): si n <= 15 => 15 - n; (15 - n) * 2
+# Ejercicio 17: Crear una función para determinar si un número es cercano a 1000 o 2000
+# Ejercicio 18: Calcular la suma de tres números, e incluir una condición de igualdad
+# Ejercicio 19: Comprobar si una cadena de caracteres termina con la extensión .py. Si no es así, agregar la extensión.
+# Ejercicio 20: Emular el funcionamiento del producto de cadenas de caracteres
+
+
+
+
+
