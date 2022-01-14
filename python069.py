06dddb5d4c67d7ac231ca22504f5baa0e7df7628
Yokekomo <albanochicharroaltur@gmail.com>
2022-01-14 14:04:29 +0100

    Actualizado

--- python069.py
+++ python069.py
@@ -4,8 +4,13 @@ numeros = input('Introduce un número positivo entero de tres cifras: ')
 lista_numeros = list(numeros)
 lista_numeros = [int(c) for c in lista_numeros]
 
+lista_numeros.sort()
+print(lista_numeros)
+
+
+"""
 min = min(lista_numeros)
 max = max(lista_numeros)
 lista_numeros = sum(lista_numeros) - min - max
 
-print('Los números ordenados: {}{}{}'.format(min, lista_numeros, max))
+print('Los números ordenados: {}{}{}'.format(min, lista_numeros, max))"""
